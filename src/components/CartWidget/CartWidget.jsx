d128495c92b1002f5dad69c0a9451785419e9e28
hernan gerez <gerez_hernan@live.com>
2023-05-31 19:50:12 -0300

    second commit

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,12 @@
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+const CartWidget = () => {
+  return (
+    <div >
+        <ShoppingCartIcon/>
+        0
+    </div>
+  )
+}
+
+export default CartWidget
